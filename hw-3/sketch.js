48a32fb0ecd5a782960a5aa014b3d1b7f4884827
Maxwell Briggs <Maxwellbriggs91@gmail.com>
2018-02-10 19:14:19 -0700

    new sketch for homework, played with bg and text

---
+++ hw-3/sketch.js
@@ -0,0 +1,14 @@
+// Declaring setup
+function setup(){
+
+  //creating a canvas 800 by 350
+  createCanvas( 800, 350 );
+
+  //coloring background as yellow
+  background('yellow');
+}
+//declaring draw functionality
+function draw(){
+  //Creating text to say Hello World at x20 y30
+  text("Hello World!", 20, 30 );
+}
