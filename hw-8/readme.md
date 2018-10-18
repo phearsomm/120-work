4f84c8d527a0ddbf6843d6585e2c37f6d5366adb
Maxwell Briggs <Maxwellbriggs91@gmail.com>
2018-10-17 22:44:33 -0600

    finished

--- hw-8/readme.md
+++ hw-8/readme.md
@@ -1,5 +1,17 @@
-# Empty p5 sketch.
+# What I Wanted To Do
+going through everything this week i saw the cycling picture that Mr. Musick had on the last page. I liked how it changed when you clicked it and stayed inside that loop. I didn't want that many and i knew i wanted the receding orbs so I decided to do three different ones.
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+## Option 1 in the if/else if statements
+Opotion 1 was similar to the perspective option in the example except the fill colors were set to separate random values based on the mouse locations. The stroke weight was set to be random between 1 and 10.
 
-Current p5 version -> 0.7.1
+## Option 2
+This one was like the arcs except i made the arcs into full circles and played with the spacing so it seemed more sparse. The same color randomization was used.
+
+## Option 3
+This was my favorite, the receding circles that grow from where the mouse is located up. this also has the random colors.
+
+## the way my colors work
+I used the code so that the background, fill, and strokes were all set to change in similar but different ways which gave the result of colorful contrast while the mouse was near the outside of the window and whiter contrast if near the center. The effect was very fun to figure out.
+
+### The options
+I used the option format that was in the example and just changed my parameters to be within 3 instead of 5.
--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -7,40 +7,52 @@ function setup() {
 }
 
 function draw() {
-	background('rgb( 45, 72, 196)');
+	background(r, 174, b);
+  //map it
   let home = map(mouseX, 0, width, 20, 50);
+    //random fills and strokes
   r = abs ((width - mouseX) % width);
   b = abs ((height - mouseY) % height);
   g = abs (((mouseX + mouseY) /2) % height );
+  fill( r, b, g);
+  stroke(b, r, g);
 
+  //create option one
   if (option == 1) {
       // Option 1
       for (let x = 50; x <= width-50; x += home) {
-        for (let y = 50; y <= height-50; b+=home) {
+        for (let y = 50; y <= height-50; y+=home) {
+          strokeWeight(random(10));
           line(x, y, width/2, height/2);
         }
       }
     }
+    // create option 2
   else if (option == 2) {
     // Option 2
+      strokeWeight(1);
     let count = 120;
     for (let x = 50; x <= width-50; x += home) {
       for (let y = 50; y <= height-50; y+=home) {
-        let s = map(count, 120, 0, 0, TWO_PI*2);
-        arc(x, y, 14, 14, s, s + PI);
+        //spacing
+        let s = map(count, 120, 0, 0, TWO_PI*0.1);
+        arc(x, y, 14, 14, s, s + TWO_PI);
         count--;
       }
     }
   }
+  // create option three
 else if (option == 3){
-	for (let y = mouseY; y <= height; y += home) {
-		for (let x = 16; x <= width; x += home) {
-			fill( r, g, b);
+  strokeWeight(random(5));
+	for (let y = mouseY; y <= height; y += 30) {
+		for (let x = 16; x <= width; x += 35) {
 			ellipse(x + y, y, 100 - y/6.0, 100 - y/6.0);
 		}
 	}
 }
 }
+
+//mouse pressed functionnnn
 function mousePressed() {
   option++;
   if (option > 3) option = 1;
